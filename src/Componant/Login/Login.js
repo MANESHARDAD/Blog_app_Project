887c57079b56b04c39700e73d6e4c914c157652c
MANESHARDAD <DK0236@digikull.com>
2022-01-16 00:48:42 +0530

    first

--- src/Componant/Login/Login.js
+++ src/Componant/Login/Login.js
@@ -3,32 +3,34 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faLock } from '@fortawesome/free-solid-svg-icons'
 
 export default function Login() {
-    return (
-        <>
+  return (
+    <>
       <div className="login-container">
         <div className="login_box">
           <div className="title1"> Login Page </div>
-          <div className="title2"> ( Mall Owner Login ) </div>
+
           <div className="field">
             <form  >
               <div className="login-content">
+                <label className="input_label">Username</label>
                 <div>
-                <FontAwesomeIcon className="fontawsome" icon={faUser} />
+                  <FontAwesomeIcon className="fontawsome" icon={faUser} />
                   <input className="login-input" type="email" placeholder="Email ID" />
                 </div>
+                <label className="input_label">Password</label>
                 <div>
-                <FontAwesomeIcon className="fontawsome" icon={faLock} />
+                  <FontAwesomeIcon className="fontawsome" icon={faLock} />
                   <input className="login-input" type="password" placeholder="Password" />
-                   
+
                 </div>
                 <button className="login_button" type="submit">Login</button>
 
               </div>
-              
+
             </form>
           </div>
         </div>
       </div>
     </>
-    )
+  )
 }
